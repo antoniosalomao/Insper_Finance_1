0265324bd102d18970184719ef3beb480ce5da2a
Antônio Salomão <a.xsalomao@outlook.com>
2020-08-27 02:11:21 -0300

    Delete ibov_cleaning_1.py

--- ibov_cleaning_1.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-ibov_excel_path = ('/Users/antonioelias/Desktop/Insper - Modules/Insper Q3/Finance 1 - Investments/Aplicacao_1/Ibovespa_Consolidado.xlsx')
-df_main = pd.read_excel(ibov_excel_path)
-print(df_main.head())
